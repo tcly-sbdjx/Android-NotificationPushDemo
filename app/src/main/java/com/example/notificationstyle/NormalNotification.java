81aa4c7759ddc541efb486c30159fba4b9eccaa2
yingleyi <yingleyi@xiaomi.com>
2023-03-13 17:49:23 +0800

    1

--- app/src/main/java/com/example/notificationstyle/NormalNotification.java
+++ app/src/main/java/com/example/notificationstyle/NormalNotification.java
@@ -13,12 +13,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class NormalNotification extends AppCompatActivity {
@@ -29,6 +34,9 @@ public class NormalNotification extends AppCompatActivity {
     private Intent mIntent;
     private PendingIntent mPendingIntent;
     private Random mRrandom;
+    private Handler mHandler;
+    private NotificationCompat.Builder notificationBuilder;
+    private Runnable mRunnable;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -36,7 +44,6 @@ public class NormalNotification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_notification);
-
         mContext=getApplicationContext();
         mNotificationManager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         mNotificationChannel=new NotificationChannel("channel","普通通知",NotificationManager.IMPORTANCE_HIGH);
@@ -44,32 +51,39 @@ public class NormalNotification extends AppCompatActivity {
         mIntent=new Intent(this,MainActivity.class);
         mPendingIntent=PendingIntent.getActivity(this,0,mIntent,PendingIntent.FLAG_IMMUTABLE);
         mRrandom=new Random();
+        mRunnable=new Runnable() {
+            @Override
+            public void run() {
+                mNotificationManager.notify(mID,notificationBuilder.build());
+                mID+=1;
+            }
+        };
+        mHandler=new Handler();
     }
 
     public void sendBasicNotification(View view) throws InterruptedException {
         int randomText=mRrandom.nextInt(10000);
-        NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(mContext,"channel")
+        int progress=0;
+        notificationBuilder=new NotificationCompat.Builder(mContext,"channel")
                 .setSmallIcon(R.drawable.image)
                 .setContentTitle("basic_"+mID)
                 .setContentIntent(mPendingIntent)
                 .setAutoCancel(true)
+                .setProgress(100,progress,false)
+                .setGroup("123")
                 .setContentText("19961226 "+randomText);
-
-        mNotificationManager.notify(mID,notificationBuilder.build());
-        mID+=1;
+        mHandler.postDelayed(mRunnable,2000);
     }
 
     public void sendFullScreenNotification(View view) throws InterruptedException {
         int randomText=mRrandom.nextInt(10000);
-        NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(mContext,"channel")
+        notificationBuilder=new NotificationCompat.Builder(mContext,"channel")
                 .setSmallIcon(R.drawable.image)
                 .setContentTitle("fullscreen_"+mID)
                 .setContentText("19961226 "+randomText)
                 .setFullScreenIntent(mPendingIntent,true);
         Toast.makeText(mContext,"请退回到桌面", Toast.LENGTH_SHORT).show();
-        Thread.sleep(2000);
-        mNotificationManager.notify(mID,notificationBuilder.build());
-        mID+=1;
+        mHandler.postDelayed(mRunnable,2000);
     }
 
     public void sendOngoingNotification(View view){
@@ -77,12 +91,24 @@ public class NormalNotification extends AppCompatActivity {
         Notification notification=new NotificationCompat.Builder(mContext,"channel")
                 .setSmallIcon(R.drawable.image)
                 .setContentTitle("ongoing_"+mID)
-                .setContentText("19961226 "+randomText)
                 .setOngoing(true)
                 .build();
         mNotificationManager.notify(mID,notification);
         mID+=1;
     }
+
+//    public void sendGroupSummary(View view){
+//        int randomText=mRrandom.nextInt(10000);
+//        Notification notification=new NotificationCompat.Builder(mContext,"channel")
+//                .setSmallIcon(R.drawable.image)
+//                .setContentTitle("groupsummary")
+//                .setContentText("groupsummary")
+//                .setGroupSummary(true)
+//                .setGroup("123")
+//                .build();
+//        mNotificationManager.notify(mID,notification);
+//        mID+=1;
+//    }
 
     public void sendMinibarNotification(View view) throws InterruptedException {
         int randomText=mRrandom.nextInt(10000);
